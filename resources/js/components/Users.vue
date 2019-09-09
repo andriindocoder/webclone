06117360aba45cca7b22192233dd79f5f3be3090
andri <andriwicaksonost@gmail.com>
2019-09-10 05:26:49 +0700

    Adding Responsive Table

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,15 +1,50 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">User Component</div>
+        <div class="row mt-5">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Users Table</h3>
 
-                    <div class="card-body">
-                        I'm a User component.
-                    </div>
+                <div class="card-tools">
+
                 </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Type</th>
+                      <th>Modify</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>183</td>
+                      <td>John Doe</td>
+                      <td>11-7-2014</td>
+                      <td><span class="tag tag-success">Approved</span></td>
+                      <td>
+                          <a href="">
+                              <i class="fa fa-edit"></i>
+                          </a>
+                           | 
+                          <a href="">
+                              <i class="fa fa-trash text-red"></i>
+                          </a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
             </div>
+            <!-- /.card -->
+          </div>
         </div>
     </div>
 </template>
