289b2d3720cdaf77c4ff119d81b900fe1797dcd2
andri <andriwicaksonost@gmail.com>
2019-09-11 11:16:18 +0700

    Pictures and Layouts

---
+++ resources/views/layouts/directaaus/head.blade.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset=utf-8>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <title>Directaa - Directory for New Australian Arrivals</title>
+    <meta name=description content="Directa is an online directory that connects migrants and refugees to information and services that helps kick-start their lives in Australia.">
+    <meta name=csrf-token content="JdWIuy91W0g7f6FeTGJVpuyprncLQy9HgLtyaPIL">
+    <link href="//fonts.googleapis.com/css?family=Nunito:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('/directaaus/css/style.css') }}">
+    <style>
+        .page_speed_1680151773 {
+            position: absolute;
+            left: -5000px;
+        }
+    </style>
+</head>
